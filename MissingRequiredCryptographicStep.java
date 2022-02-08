1903e7a89cf02db9f852135d52247455bc25a39f
Manasi1313 <76029071+Manasi1313@users.noreply.github.com>
2022-02-08 15:47:37 +0530

    Update MissingRequiredCryptographicStep.java

--- MissingRequiredCryptographicStep.java
+++ MissingRequiredCryptographicStep.java
@@ -1,36 +1,34 @@
-package javacodechecker;
 
-public class MissingRequiredCryptographicStep{
-	public void notValid() throws Throwable
-    {
-       
-            final String CIPHER_INPUT = "ABCDEFG123456";
-            // EMB-ISSUE: CodeIssueNames.MISSING_REQUIRED_CRYPTOGRAPHIC_STEP
-            KeyGenerator kG = KeyGenerator.getInstance("AES");
-            SecretKey secretKey = kG.generateKey();
-            byte[] byteKey = secretKey.getEncoded();
-            SecretKeySpec secretKeySpec = new SecretKeySpec(byteKey, "AES");
-            Cipher aesCipher = Cipher.getInstance("AES");
-            aesCipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
-            byte[] encrypted = aesCipher.doFinal(CIPHER_INPUT.getBytes("UTF-8"));
-            IO.writeLine(IO.toHex(encrypted));
-            
-        
-    }
-	 private void Valid() throws Throwable
-	    {
-	        final String CIPHER_INPUT = "ABCDEFG123456";
-	            KeyGenerator kGVal = KeyGenerator.getInstance("AES");
-	            // EMB-ISSUE: CodeIssueNames.MISSING_REQUIRED_CRYPTOGRAPHIC_STEP/no-detect
-	            kGVal.init(256);
-	            SecretKey secretKey = kGVal.generateKey();
-	            byte[] byteKey = secretKey.getEncoded();
-	            SecretKeySpec secretKeySpec = new SecretKeySpec(byteKey, "AES");
-	            Cipher aesCipher = Cipher.getInstance("AES");
-	            aesCipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
-	            byte[] encrypted = aesCipher.doFinal(CIPHER_INPUT.getBytes("UTF-8"));
-	            IO.writeLine(IO.toHex(encrypted));
-	           
-	        }
-	  
- }
+package javacodechecker;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
+public class MissingRequiredCryptographicStep {
+	public void CryptographicStep()  throws Throwable{
+
+		// EMB-ISSUE: CodeIssueNames.MISSING_REQUIRED_CRYPTOGRAPHIC_STEP
+		KeyGenerator kG = KeyGenerator.getInstance("AES");
+		SecretKey secretKey = kG.generateKey();
+
+	}
+
+	private void CryptographicStep2()  throws Throwable{
+
+		// EMB-ISSUE: CodeIssueNames.MISSING_REQUIRED_CRYPTOGRAPHIC_STEP
+		KeyGenerator kGVal = KeyGenerator.getInstance("AES");
+		kGVal.equals(kGVal);
+		SecretKey secretKeyy = kGVal.generateKey();
+
+	}
+
+	private void CryptographicStep3() {
+
+		// EMB-ISSUE: CodeIssueNames.MISSING_REQUIRED_CRYPTOGRAPHIC_STEP/no-detect
+		KeyGenerator kGVal1 = KeyGenerator.getInstance("AES");
+		kGVal1.init(256);
+		SecretKey secretKey1 = kGVal1.generateKey();
+
+	}
+
+}
+
