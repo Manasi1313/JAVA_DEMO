a9a9c0e7af62e1e4033ed1928eb62de5a4afedb4
Manasi1313 <76029071+Manasi1313@users.noreply.github.com>
2022-02-08 15:11:14 +0530

    Update ShortcircuitLogicShouldBeUsedInBooleanContexts.java

--- ShortcircuitLogicShouldBeUsedInBooleanContexts.java
+++ ShortcircuitLogicShouldBeUsedInBooleanContexts.java
@@ -4,40 +4,40 @@ public class ShortcircuitLogicShouldBeUsedInBooleanContexts {
 	public void logic() {
 		// EMB-ISSUE: CodeIssueNames.SHORTCIRCUIT_LOGIC_SHOULD_BE_USED_IN_BOOLEAN_CONTEXTS
 		if(getTrue() | getFalse()) {
-		System.out.println("This output"+"gets print");
+		logger.info("This output"+"gets print");
         }
         else {
-        System.out.println("This output"+"will not be printed");
+       logger.info("This output"+"will not be printed");
         }
 		}
 	
 	public void logic1() {
 		// EMB-ISSUE: CodeIssueNames.SHORTCIRCUIT_LOGIC_SHOULD_BE_USED_IN_BOOLEAN_CONTEXTS/no-detect
 		if(getTrue() || getFalse()) {
-			System.out.println("This output"+"gets print");
+			logger.info("This output"+"gets print");
 	        }
 	        else {
-	        System.out.println("This output"+"will not be printed");
+	       logger.info("This output"+"will not be printed");
 	        }
 			 
 		}
 	public void logic2() {
 		// EMB-ISSUE: CodeIssueNames.SHORTCIRCUIT_LOGIC_SHOULD_BE_USED_IN_BOOLEAN_CONTEXTS
 		if(getTrue() & getFalse()) {
-		System.out.println("This output"+"gets print");
+		logger.info("This output"+"gets print");
         }
         else {
-        System.out.println("This output"+"will not be printed");
+        logger.info("This output"+"will not be printed");
         }
 		}
 	
 	public void logic3() {
 		// EMB-ISSUE: CodeIssueNames.SHORTCIRCUIT_LOGIC_SHOULD_BE_USED_IN_BOOLEAN_CONTEXTS/no-detect
 		if(getTrue() && getFalse()) {
-			System.out.println("This output"+"gets print");
+			logger.info("This output"+"gets print");
 	        }
 	        else {
-	        System.out.println("This output"+"will not be printed");
+	        logger.info("This output"+"will not be printed");
 	        }
 			 
 		}
