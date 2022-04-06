aafa2f5964805a2c7cae05a7ebd1f48cb5e42445
Manasi1313 <76029071+Manasi1313@users.noreply.github.com>
2022-04-06 11:11:06 +0530

    Update ComparisonOfObjectReferencesInsteadOfObjectContents.java

--- ComparisonOfObjectReferencesInsteadOfObjectContents.java
+++ ComparisonOfObjectReferencesInsteadOfObjectContents.java
@@ -1,11 +1,10 @@
 package javacodechecker;
 
-public class ComparisonOfObjectReferencesInsteadOfObjectContents {
+public class ComparisionOfObjectReferencesInsteadOfObjectContents {
 	String str1 = new String("Hello");
 	String str2 = new String("Hello");
-
 	public void compare() {
-		// EMB-ISSUE: CodeIssueNames.COMPARISON_OF_OBJECT_REFRENCE_INSTEAD_OF_OBJECT_CONTENTS
+		// EMB-ISSUE: CodeIssueNames.COMPARISION_OF_OBJECT_REFRENCE_INSTEAD_OF_OBJECT_CONTENTS
 		if (str1 == str2) {
 			logger.info("str1 == str2");
 		}
@@ -15,16 +14,35 @@ public class ComparisonOfObjectReferencesInsteadOfObjectContents {
 		compareName(name, myname);
 	}
 	
+	
 	public void compareName(String name,String myname) {
-		// EMB-ISSUE: CodeIssueNames.COMPARISON_OF_OBJECT_REFRENCE_INSTEAD_OF_OBJECT_CONTENTS
+		// EMB-ISSUE: CodeIssueNames.COMPARISION_OF_OBJECT_REFRENCE_INSTEAD_OF_OBJECT_CONTENTS
 		if(name == myname) {
-			logger.info("name == myname");		}
+			logger.info("name == myname");		
+			}
 	}
 	
+	public void compareInMethods() {
+		String strs1 = new String("Hey");
+		String strs2 = new String("Hey");
+		// EMB-ISSUE: CodeIssueNames.COMPARISION_OF_OBJECT_REFRENCE_INSTEAD_OF_OBJECT_CONTENTS
+		if(strs1 == strs2) {
+			logger.info("strs1 == strs2");		
+			}
+	}
+	
+	public void compareInsideMethods() {
+		String st = new String("Hey");
+		String st1 = new String("Hey");
+		// EMB-ISSUE: CodeIssueNames.COMPARISION_OF_OBJECT_REFRENCE_INSTEAD_OF_OBJECT_CONTENTS
+		if(st1 == st || st1 != st) {
+			logger.info("sts1 == sts");		
+			}
+	}
 	
 
 	public void compares() {
-		// EMB-ISSUE: CodeIssueNames.COMPARISON_OF_OBJECT_REFRENCE_INSTEAD_OF_OBJECT_CONTENTS/no-detect
+		// EMB-ISSUE: CodeIssueNames.COMPARISION_OF_OBJECT_REFRENCE_INSTEAD_OF_OBJECT_CONTENTS/no-detect
 		if (str1.equals(str2)) {
 			logger.info("str1 equals str2");
 		}
