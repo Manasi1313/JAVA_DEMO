1e156730a8a22d7ee417794fb1f23465ba672016
Manasi1313 <76029071+Manasi1313@users.noreply.github.com>
2022-07-19 12:29:03 +0530

    Create NotifyAllShouldBeUsed1.java

---
+++ NotifyAllShouldBeUsed1.java
@@ -0,0 +1,10 @@
+package javacodechecker;
+public	class NotifyAllShouldBeUsed1  implements Runnable{
+    @Override
+		  public void runs(){
+		    synchronized(this){
+		    	// EMB-ISSUE: CodeIssueNames.NOTIFYALL_SHOULD_BE_USED/no-detect
+		    	notifyAll();   
+		    }
+		  }
+		}
