05d731861b8254dd2bfd1753c4e8513fe2bc6bc0
Manasi1313 <76029071+Manasi1313@users.noreply.github.com>
2022-07-19 11:47:54 +0530

    Update OnStringObjecttoStringShouldNeverBeCalled.java

--- OnStringObjecttoStringShouldNeverBeCalled.java
+++ OnStringObjecttoStringShouldNeverBeCalled.java
@@ -13,12 +13,17 @@ public class OnStringObjecttoStringShouldNeverBeCalled {
 	// EMB-ISSUE: CodeIssueNames.ON_STRING_OBJECT_TOSTRING_SHOULD_NEVER_BE_CALLED/no-detect
 	System.out.println(messages);
 	}
-	public void mds() {
+	public void method2() {
 		// EMB-ISSUE: CodeIssueNames.ON_STRING_OBJECT_TOSTRING_SHOULD_NEVER_BE_CALLED
 		System.out.println(msg.toString());
 	}
-	public void md() {
+	public void method3() {
 		// EMB-ISSUE: CodeIssueNames.ON_STRING_OBJECT_TOSTRING_SHOULD_NEVER_BE_CALLED/no-detect
 		System.out.println(msg);
+	}
+	public void method4(){
+	messages = "HEY THERE";
+	// EMB-ISSUE: CodeIssueNames.ON_STRING_OBJECT_TOSTRING_SHOULD_NEVER_BE_CALLED/no-detect
+	System.out.println(messages);
 	}
 }
